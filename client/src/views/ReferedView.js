713121e3f1aa0a1771a691c5637dd3f83fa8173f
madhav2391 <psvsaimadhav@gmail.com>
2022-02-25 22:58:30 +0530

    list button

--- client/src/views/ReferedView.js
+++ client/src/views/ReferedView.js
@@ -1,44 +1,46 @@
-import React, { useState } from "react";
-import MUIDataTable from "mui-datatables";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-export const ReferedView = ({apiData}) => {
-    const columns = [
-        { label: "Name",name:"name", options: { filterOptions: { fullWidth: true } ,
-        customBodyRenderLite: (dataIndex) => {
-            let val = apiData[dataIndex].name;
-            return val;
-          } } 
-        },
-        {label:"Role",name:"role",options: { filterOptions: { fullWidth: true } ,
-        customBodyRenderLite: (dataIndex) => {
-            let val = apiData[dataIndex].role;
-            return val;
-          } } },
-        {name:"orgName",label:"Organisation Name",        
-        options: { filterOptions: { fullWidth: true } ,
-        customBodyRenderLite: (dataIndex) => {
-            let val = apiData[dataIndex].name;
-            return val;
-          } } 
-        },
-    ];
-    
-      const options = {
-        search: true,
-        download: false,
-        print: false,
-        viewColumns: true,
-        filter: true,
-        filterType: "dropdown",
-        responsive:"standard",
-        selectableRows:"none",
-      };
-    return(
-        <MUIDataTable
-        title={"users list"}
-        data={apiData}
-        columns={columns}
-        options={options}
-      />
-    );
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4
+};
+
+export const ReferedView = ({shouldOpen, data, handleClose})  => {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+
+//   React.useEffect(() => {
+//     setOpen(shouldOpen)
+//   }, [data, shouldOpen]);
+
+  return (
+    <div>
+      <Modal
+        open={shouldOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+           {data.name}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {data.role}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
