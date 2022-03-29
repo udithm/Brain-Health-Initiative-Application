59ca4f7634de61b312934641988c6373ddd74a19
superhakar <43088948+superhakar@users.noreply.github.com>
2022-03-29 11:45:18 +0530

    Fixed My profile

--- client/src/services/MyProfileApi.js
+++ client/src/services/MyProfileApi.js
@@ -19,29 +19,29 @@ export const myProfile = (uId, history) => {
     const id = uId;
     return (dispatch) => {
         dispatch(myProfileRequest());
-        // axios
-        //     .post ("/myProfile", {id}) // /login/authentication part will be attached to base url
-        //     .then ((res) => {
-        //         console.log("-------this is then------- ", res);
+        axios
+            .post ("/myProfile", {id}) // /login/authentication part will be attached to base url
+            .then ((res) => {
+                console.log("-------this is then------- ", res);
 
-        //         if (!res.data) { throw new Error("Profile fetch unsucessful.");} // this is added so mock server can be used(jugad)
-        //         localStorage.setItem("userId", id);
-        //         console.log("-------this is then222------- ", res.data);
+                if (!res.data) { throw new Error("Profile fetch unsucessful.");} // this is added so mock server can be used(jugad)
+                localStorage.setItem("userId", id);
+                console.log("-------this is then222------- ", res.data);
 
-        //         dispatch(myProfileSuccess(res.data));
-        //         console.log("-------this is then33333333333333------- ", res);
+                dispatch(myProfileSuccess(res.data));
+                console.log("-------this is then33333333333333------- ", res);
 
-        //         if (history) {
-        //             history.push("/myProfile");
-        //             // dispatch(alertSuccess("Profile fetch Successful!"));
-        //         }
-        //     })
-        //     .catch((err)=>{
-        //         dispatch(myProfileFailure(err.message));
-        //         dispatch(alertError(err.message));
-        //         console.log("-----this is catch------", err);
+                if (history) {
+                    history.push("/myProfile");
+                    // dispatch(alertSuccess("Profile fetch Successful!"));
+                }
+            })
+            .catch((err)=>{
+                dispatch(myProfileFailure(err.message));
+                dispatch(alertError(err.message));
+                console.log("-----this is catch------", err);
 
-        //     })
+            })
     }
 
 }
