51e3567e85118c050dc0b7e8488b79a68ab17a86
keshav <keshavsinghal594@gmail.com>
2022-04-20 13:54:58 +0530

    epilapsy backend

--- Server/src/main/java/com/example/demo/services/QuestionnaireResponseService.java
+++ Server/src/main/java/com/example/demo/services/QuestionnaireResponseService.java
@@ -23,20 +23,13 @@ public class QuestionnaireResponseService {
             String yes = "Yes";
             if (answers.get("Q1").equals(yes) || answers.get("Q2").equals(yes) || answers.get("Q3").equals(yes)) {
                 ret.put("nextQuestionnaire", "Epilepsy1");
-                return ret;
-            }
-            if (answers.get("Q4").equals(yes) || answers.get("Q5").equals(yes) || answers.get("Q6").equals(yes)
+            } else if (answers.get("Q4").equals(yes) || answers.get("Q5").equals(yes) || answers.get("Q6").equals(yes)
                     || answers.get("Q7").equals(yes) || answers.get("Q8").equals(yes)) {
                 ret.put("nextQuestionnaire", "Stroke1");
-                return ret;
-            }
-            if (answers.get("Q11").equals(yes) || answers.get("Q12").equals(yes)) {
+            } else if (answers.get("Q11").equals(yes) || answers.get("Q12").equals(yes)) {
                 ret.put("nextQuestionnaire", "Headache");
-                return ret;
-            }
-            if (answers.get("Q9").equals(yes) || answers.get("Q13").equals(yes)) {
+            } else if (answers.get("Q9").equals(yes) || answers.get("Q13").equals(yes)) {
                 ret.put("nextQuestionnaire", "Dementia");
-                return ret;
             }
         } else if (questionnaireResponse.getQuestionnaire().getName().equals("Stroke1")) {
             Map<String, String> answers = questionnaireResponse.getAnswers();
@@ -46,7 +39,69 @@ public class QuestionnaireResponseService {
                     || answers.get("Q7").equals(yes) || answers.get("Q8").equals(yes)) {
                 ret.put("Diagnosis", "Stroke");
                 ret.put("Referral", "Secondary");
-                return ret;
+            }
+        } else if (questionnaireResponse.getQuestionnaire().getName().equals("Epilepsy1")) {
+            Map<String, String> answers = questionnaireResponse.getAnswers();
+            String yes = "Yes";
+            int cnt = 0;
+            int i = 1;
+            for (String answer : answers.values()) {
+                if (answer.equals(yes)) {
+                    cnt += 1;
+                }
+
+                i++;
+                if (i > 6) {
+                    break;
+                }
+            }
+            if (cnt >= 4) {
+                if (Integer.parseInt(answers.get("age")) < 10) {
+                    if (answers.get("Q9").equals(yes)) {
+                        ret.put("nextQuestionnaire", "Apx4");
+                    } else {
+                        ret.put("nextQuestionnaire", "Apx5");
+                    }
+                } else {
+                    if (answers.get("Q8").equals(yes)) {
+                        ret.put("Diagnosis", "Epilepsy");
+                    } else {
+                        ret.put("Diagnosis", "Uncertain for Epilepsy");
+                    }
+                }
+            } else if (cnt <= 3) {
+                if (answers.get("Q7").equals(yes)) {
+                    if (answers.get("Q8").equals(yes)) {
+                        ret.put("Diagnosis", "Epilepsy");
+                    } else {
+                        ret.put("Diagnosis", "Uncertain for Epilepsy");
+                    }
+                } else {
+                    ret.put("Diagnosis", "uncertain for seizure");
+                }
+            }
+        } else if (questionnaireResponse.getQuestionnaire().getName().equals("Apx4")) {
+            Map<String, String> answers = questionnaireResponse.getAnswers();
+            String yes = "Yes";
+            if (answers.get("Q1").equals(yes) || answers.get("Q2").equals(yes) || answers.get("Q5").equals(yes)
+                    || answers.get("Q6").equals(yes)) {
+                ret.put("Diagnosis", "Typical absence seizures");
+            } else {
+                if (answers.get("Q3").equals(yes) || answers.get("Q4").equals(yes)) {
+                    ret.put("Diagnosis", "Probable absence seizures");
+                } else {
+                    ret.put("Diagnosis", "Generalized seizure");
+                }
+            }
+        } else if (questionnaireResponse.getQuestionnaire().getName().equals("Apx5")) {
+            Map<String, String> answers = questionnaireResponse.getAnswers();
+            String yes = "Yes";
+            if ((answers.get("Q1").equals(yes) || answers.get("Q2").equals(yes) || answers.get("Q3").equals(yes)
+                    || answers.get("Q4").equals(yes) || answers.get("Q5").equals(yes) || answers.get("Q6").equals(yes))
+                    && Integer.parseInt(answers.get("age")) < 5) {
+                ret.put("Diagnosis", "Typical febrile seizure");
+            } else {
+                ret.put("Diagnosis", "Atypical febrile seizure");
             }
         }
         return ret;
